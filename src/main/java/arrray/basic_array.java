eca452ceaee7628807b951323c496a6ad233b9a0
edan <sahedan.program@gmail.com>
2021-11-09 14:38:43 +0800

    Changes to be committed: 	new file:   src/main/java/arrray/basic_array.java 	new file:   target/classes/arrray/basic_array.class

---
+++ src/main/java/arrray/basic_array.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrray;
+
+/**
+ *
+ * @author ed
+ */
+//public class unittest {
+    
+
+
+class basic_array {
+ public static void main(String[] args) {
+  
+   // create an array
+   int[] age = {12, 4, 5, 2, 5};
+
+   // access each array elements
+   System.out.println("Accessing Elements of Array:");
+   System.out.println("First Element: " + age[0]);
+   System.out.println("Second Element: " + age[1]);
+   System.out.println("Third Element: " + age[2]);
+   System.out.println("Fourth Element: " + age[3]);
+   System.out.println("Fifth Element: " + age[4]);
+ }
+ 
+}
+
+
